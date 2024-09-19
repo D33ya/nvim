f0e30f915010ac1b58c5b5b8aaa5e13f332af767
d33ya <D33ya@proton.me>
2024-09-19 15:31:34 -0500

    updated keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -23,17 +23,15 @@ vim.keymap.set('v', '<leader>s', function()
   require('toggleterm').send_lines_to_terminal('visual_selection', trim_spaces, { args = vim.v.count })
 end)
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Resize windows
+vim.keymap.set('n', '<C-Down>', '<cmd>resize -2<CR>')
+vim.keymap.set('n', '<C-Up>', '<cmd>resize +2<CR>')
+vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>')
